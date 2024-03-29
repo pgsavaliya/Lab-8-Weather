7f8e434c7b32c6388e53bfb9dc8446edd91fb806
Pgsavaliya <pgsavaliya01@gmail.com>
2024-03-29 12:57:56 -0400

    Initial Commit

---
+++ Lab 8 Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab 8 Weather
+//
+//  Created by Pavan savaliya on 2024-03-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lab 8 WeatherUITests/Lab_8_WeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab_8_WeatherUITestsLaunchTests.swift
+//  Lab 8 WeatherUITests
+//
+//  Created by Pavan savaliya on 2024-03-29.
+//
+
+import XCTest
+
+final class Lab_8_WeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
